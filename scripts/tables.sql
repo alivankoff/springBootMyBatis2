f846318f7ad10430e654272b49bd739cf7c77542
Bharat Lal <Bharat.Lal@walmart.com>
2017-10-07 22:00:24 +0530

    SpringBoot + Mybatis Example

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -6,3 +6,6 @@ CREATE TABLE spring.COURSE ( id int unsigned not null auto_increment,
                              CREATED_TIME DATETIME DEFAULT CURRENT_TIMESTAMP,
                              UPDATED_TIME DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                              constraint pk_example primary key (id) );
+
+
+INSERT INTO spring.COURSE(NAME, DESCRIPTION, CREDITS, PROFESSOR) VALUES ('Machine Learning', 'Machine learning is a field of computer science that gives computers the ability to learn without being explicitly programmed.', '5.0', 'Andrew N G')
